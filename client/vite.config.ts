cafdf0c64077aa54218937e784127abd0642558a
Sam Silver <sam55silver@gmail.com>
2024-11-13 15:42:02 -0400

    Add in react client and python server

---
+++ client/vite.config.ts
@@ -0,0 +1,26 @@
+import path from "path"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import Unfonts from 'unplugin-fonts/vite';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    Unfonts({
+      custom: {
+        families: [
+          {
+            name: 'Geist',
+            src: './src/assets/fonts/geist/*.woff2',
+          },
+        ],
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+})

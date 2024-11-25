09740a15a6cbf2f2b46efd53b24f61b12a09f324
Sam Silver <sam55silver@gmail.com>
2024-11-24 20:36:16 -0400

    Add in some more tutorial stuff

--- server/lib/agent.py
+++ server/lib/agent.py
@@ -1,13 +1,20 @@
+from langchain_core.runnables import RunnableConfig
 from langchain_openai import ChatOpenAI
-from langchain_core.messages import HumanMessage
 
 from typing import Annotated
 
 from typing_extensions import TypedDict
 
-from langgraph.graph import StateGraph, START, END
+from langgraph.graph import StateGraph
 from langgraph.graph.message import add_messages
+from langgraph.prebuilt import ToolNode, tools_condition
+from langchain_core.tools import tool
+from langgraph.checkpoint.memory import MemorySaver
 
+@tool
+def multiply(a: int, b: int) -> int:
+    """Multiple two numbers together."""
+    return a * b
 
 class State(TypedDict):
     # Messages have the type "list". The `add_messages` function
@@ -15,25 +22,39 @@ class State(TypedDict):
     # (in this case, it appends messages to the list, rather than overwriting them)
     messages: Annotated[list, add_messages]
 
+memory = MemorySaver()
+
 
 graph_builder = StateGraph(State)
 
 # from openai import OpenAI
 llm = ChatOpenAI(model="gpt-4o-mini")
+llm_w_tools = llm.bind_tools([multiply])
 
 def chatbot(state: State):
-    return {"messages": [llm.invoke(state["messages"])]}
+    return {"messages": [llm_w_tools.invoke(state["messages"])]}
 
 graph_builder.add_node("chatbot", chatbot)
-graph_builder.add_edge(START, "chatbot")
-graph_builder.add_edge("chatbot", END)
+tool_node = ToolNode(tools=[multiply])
+graph_builder.add_node("tools", tool_node)
 
-graph = graph_builder.compile()
+graph_builder.add_conditional_edges(
+    "chatbot",
+    tools_condition,
+)
+
+graph_builder.add_edge("tools", "chatbot")
+graph_builder.set_entry_point("chatbot")
+graph = graph_builder.compile(checkpointer=memory)
+
+config = RunnableConfig(configurable={"thread_id": "1"})
 
 def stream_graph_updates(user_input: str):
-    for event in graph.stream({"messages": [("user", user_input)]}):
-        for value in event.values():
-            print("Assistant:", value["messages"][-1].content)
+    events = graph.stream(
+        {"messages": [("user", user_input)]}, config, stream_mode="values"
+    )
+    for event in events:
+        event["messages"][-1].pretty_print()
 
 
 while True:
